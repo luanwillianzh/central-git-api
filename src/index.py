2bc4a3336c307a67f41125ab337862cb0f35be33
luanwillianzh <74968256+luanwillianzh@users.noreply.github.com>
2025-04-03 10:16:29 -0300

    Update index.py

--- src/index.py
+++ src/index.py
@@ -30,4 +30,7 @@ def get_chapter(novel, chapter):
 @app.get("/search/{text}")
 def search(text):
     resp = requests.get("https://raw.githubusercontent.com/luanwillianzh/Novel-Reader-Data/refs/heads/main/info.json").json()
-    return {"sucesso": true, "resultado": [ {"nome": resp[novel_id]["nome"], "url": novel_id, "cover": resp[novel_id]["cover"]} for novel_id in resp if urllib.parse.unquote_plus(text.lower()) in str(resp[novel_id]).lower() ]}
+    try:
+      return {"sucesso": true, "resultado": [ {"nome": resp[novel_id]["nome"], "url": novel_id, "cover": resp[novel_id]["cover"]} for novel_id in resp if urllib.parse.unquote_plus(text.lower()) in str(resp[novel_id]).lower() ]}
+    except:
+      return {"sucesso": true, "resultado": [ {"nome": resp[novel_id]["nome"], "url": novel_id, "cover": resp[novel_id]["cover"]} for novel_id in resp ]}
